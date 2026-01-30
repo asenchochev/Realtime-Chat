bb8f36167ca5c7ffa47c3d837476cc3ed09cf26a
Asen Chochev <asen.qkiqt145@gmail.com>
2026-01-30 16:40:58 +0200

    Update HTTP CONFIG

--- backend/src/config/http.config.ts
+++ backend/src/config/http.config.ts
@@ -1,11 +1,11 @@
-export const HTTPStatus = {
-    OK: 200,
-    CREATED: 201,
-    NO_CONTENT: 204,
-    BAD_REQUEST: 400,
-    UNAUTHORIZED: 401,
-    FORBIDDEN: 403,
-    NOT_FOUND: 404,
-    INTERNAL_SERVER_ERROR: 500,
-    CONFLICT: 409,
-} as const
+export const HTTPSTATUS = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+} as const;
+
+export type HttpStatusCodeType = (typeof HTTPSTATUS)[keyof typeof HTTPSTATUS];
