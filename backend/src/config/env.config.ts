fd42267c4ca9089359d330b6b5bae544f1cc8980
Asen Chochev <asen.qkiqt145@gmail.com>
2026-01-30 16:35:06 +0200

    Env config

---
+++ backend/src/config/env.config.ts
@@ -0,0 +1,10 @@
+import { getEnv } from "../utils/get-env";
+
+export const Env = {
+    PORT: getEnv("PORT", "8000"),
+    MONGO_URI: getEnv("MONGO_URI"),
+    JWT_SECRET: getEnv("JWT_SECRET"),
+    NODE_ENV: getEnv("NODE_ENV", "development"),
+    JWT_EXPIRES_IN: getEnv("JWT_EXPIRES_IN", "1d"),
+
+} as const;
